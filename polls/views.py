c950a5c0377247693a076548cd2e2741bbb667b1
lalongooo <hdez.jeduardo@gmail.com>
2016-10-15 19:17:07 -0500

    Remove duplicate  method

--- polls/views.py
+++ polls/views.py
@@ -21,10 +21,6 @@ def results(request, question_id):
     return HttpResponse(response % question_id)
 
 def vote(request, question_id):
-    return HttpResponse("You're voting on question %s." % question_id)
-
-
-def vote(request, question_id):
     question = get_object_or_404(Question, pk=question_id)
     try:
         selected_choice = question.choice_set.get(pk=request.POST['choice'])
